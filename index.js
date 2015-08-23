460e4466aef7e73dab0ebe55273df2b6a5443e7f
Andrey Sitnik <andrey@sitnik.ru>
2015-08-23 15:23:23 +0300

    Use PostCSS 5.0 API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-will-change', function () {
     return function (css) {
-        css.eachDecl('will-change', function (decl) {
+        css.walkDecls('will-change', function (decl) {
             decl.cloneBefore({ prop: 'backface-visibility', value: 'hidden' });
         });
     };

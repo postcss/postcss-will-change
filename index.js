12adb6bf8083e3c6e6230819d8e4e162970ebcb4
Andrey Sitnik <andrey@sitnik.ru>
2018-09-07 16:33:22 +0300

    Update dependencies

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var postcss = require('postcss');
+var postcss = require('postcss')
 
 module.exports = postcss.plugin('postcss-will-change', function () {
-    return function (css) {
-        css.walkDecls('will-change', function (decl) {
-            var already = decl.parent.some(function (i) {
-                return i.type === 'decl' && i.prop === 'backface-visibility';
-            });
-            if ( already ) return;
-            decl.cloneBefore({ prop: 'backface-visibility', value: 'hidden' });
-        });
-    };
-});
+  return function (css) {
+    css.walkDecls('will-change', function (decl) {
+      var already = decl.parent.some(function (i) {
+        return i.type === 'decl' && i.prop === 'backface-visibility'
+      })
+      if (already) return
+      decl.cloneBefore({ prop: 'backface-visibility', value: 'hidden' })
+    })
+  }
+})
--- index.test.js
+++ index.test.js
@@ -1,28 +1,33 @@
-var postcss = require('postcss');
+var postcss = require('postcss')
 
-var plugin = require('./');
+var plugin = require('./')
 
-function run(input, output) {
-    return postcss([ plugin ]).process(input).then(result => {
-        expect(result.css).toEqual(output);
-        expect(result.warnings().length).toBe(0);
-    });
+function run (input, output) {
+  return postcss([plugin]).process(input, { from: undefined })
+    .then(function (result) {
+      expect(result.css).toEqual(output)
+      expect(result.warnings()).toHaveLength(0)
+    })
 }
 
-it('adds 3D hacks', () => {
-    return run('a{ will-change: transform; }',
-               'a{ backface-visibility: hidden; will-change: transform; }');
-});
+it('adds 3D hacks', function () {
+  return run(
+    'a{ will-change: transform; }',
+    'a{ backface-visibility: hidden; will-change: transform; }'
+  )
+})
 
-it('does not override existing properties', () => {
-    return run('a{ backface-visibility: visible; will-change: transform; }',
-               'a{ backface-visibility: visible; will-change: transform; }');
-});
+it('does not override existing properties', function () {
+  return run(
+    'a{ backface-visibility: visible; will-change: transform; }',
+    'a{ backface-visibility: visible; will-change: transform; }'
+  )
+})
 
-it('does not get confused by other selectors', () => {
-    var input  = 'a{ backface-visibility: visible; } ' +
-                 'b { will-change: transform; }';
-    var output = 'a{ backface-visibility: visible; } ' +
-                 'b { backface-visibility: hidden; will-change: transform; }';
-    return run(input, output);
-});
+it('does not get confused by other selectors', function () {
+  var input = 'a{ backface-visibility: visible; } ' +
+              'b { will-change: transform; }'
+  var output = 'a{ backface-visibility: visible; } ' +
+               'b { backface-visibility: hidden; will-change: transform; }'
+  return run(input, output)
+})

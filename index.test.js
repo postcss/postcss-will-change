d65a75ebf30219d0a574e323d434121f3293d0c1
Andrey Sitnik <andrey@sitnik.ru>
2022-12-08 00:07:25 +0300

    Update dev tools

--- index.test.js
+++ index.test.js
@@ -1,31 +1,35 @@
+let { equal } = require('uvu/assert')
+let { test } = require('uvu')
 let postcss = require('postcss')
 
 let plugin = require('./')
 
-async function run (input, output) {
+async function run(input, output) {
   let result = await postcss([plugin]).process(input, { from: undefined })
-  expect(result.css).toEqual(output)
-  expect(result.warnings()).toHaveLength(0)
+  equal(result.css, output)
+  equal(result.warnings().length, 0)
 }
 
-it('adds 3D hacks', async () => {
+test('adds 3D hacks', async () => {
   await run(
     'a{ will-change: transform; }',
     'a{ backface-visibility: hidden; will-change: transform; }'
   )
 })
 
-it('does not override existing properties', async () => {
+test('does not override existing properties', async () => {
   await run(
     'a{ backface-visibility: visible; will-change: transform; }',
     'a{ backface-visibility: visible; will-change: transform; }'
   )
 })
 
-it('does not get confused by other selectors', async () => {
+test('does not get confused by other selectors', async () => {
   await run(
     'a{ backface-visibility: visible; } b { will-change: transform; }',
     'a{ backface-visibility: visible; } ' +
       'b { backface-visibility: hidden; will-change: transform; }'
   )
 })
+
+test.run()
